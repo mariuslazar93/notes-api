a220bdd86df4e86938e0282c52a428d201d39421
Marius Lazar <mariuslazar93@yahoo.ro>
2020-04-01 12:59:12 +0300

    enabled cors

--- app/routes.js
+++ app/routes.js
@@ -9,25 +9,50 @@ module.exports = [
     method: 'GET',
     path: '/notes',
     handler: getAllNotesController,
+    options: {
+      cors: {
+        origin: ['localhost:4200', 'ng-notes-app.netlify.app']
+      }
+    }
   },
   {
     method: 'POST',
     path: '/notes',
     handler: createNoteController,
+        options: {
+      cors: {
+        origin: ['localhost:4200', 'ng-notes-app.netlify.app']
+      }
+    }
   },
   {
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteController,
+        options: {
+      cors: {
+        origin: ['localhost:4200', 'ng-notes-app.netlify.app']
+      }
+    }
   },
   {
     method: 'PATCH',
     path: '/notes/{id}',
     handler: updateNoteController,
+        options: {
+      cors: {
+        origin: ['localhost:4200', 'ng-notes-app.netlify.app']
+      }
+    }
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
     handler: deleteNoteController,
+        options: {
+      cors: {
+        origin: ['localhost:4200', 'ng-notes-app.netlify.app']
+      }
+    }
   },
 ];
